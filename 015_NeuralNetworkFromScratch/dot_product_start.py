bcd2914eb8432003d80c31b3ce7796b79b1026e8
Yakov Keselman <yk@helvetiaeducation.ch>
2025-06-26 19:55:43 -0500

    Tweak

--- 015_NeuralNetworkFromScratch/dot_product_start.py
+++ 015_NeuralNetworkFromScratch/dot_product_start.py
@@ -1,4 +1,4 @@
-#%% packages
+# %% packages
 import numpy as np
 # %%
 X = [0, 1]
@@ -6,3 +6,4 @@ w1 = [2, 3]
 w2 = [0.4, 1.8]
 
 # %% Question: which weight is more similar to input data X
+print('w1' if np.dot(X, w1) > np.dot(X, w2) else 'w2')
